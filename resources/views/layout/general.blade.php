2b96a6e47fd35855adb7161e26d160efb1164074
JFCreategica <juanfelipe@creategicalatina.com>
2020-10-19 17:41:37 -0500

    tooltip de los botones

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -18,7 +18,7 @@
         <?php echo '<script type="text/javascript" src = "js/dataTables.js?v='.date("Y-m-d H:i:s").'"></script>';?>
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <?php echo "<link rel='stylesheet' href='css/generalPage.css?v=".date("Y-m-d H:i:s")."'>";?>
-        
+
         <?php echo '<script type="text/javascript" src = "js/jquery-ui.min.js?v='.date("Y-m-d H:i:s").'"></script>';?>
         <?php echo '<script type="text/javascript" src = "js/config.js?v='.date("Y-m-d H:i:s").'"></script>';?>
 
@@ -29,16 +29,16 @@
         <?php echo '<script type="text/javascript" src = "js/jquery-ui.min.js?v='.date("Y-m-d H:i:s").'"></script>';?>
         <?php echo '<script type="text/javascript" src = "js/bootstrap-datepicker.js?v='.date("Y-m-d H:i:s").'"></script>';?>
         <?php echo '<script type="text/javascript" src = "js/jquery.creditCardValidator.js?v='.date("Y-m-d H:i:s").'"></script>';?>
-        
+
         <style>
-            
+
         </style>
     </head>
     <body>
         {{ csrf_field() }}
         @yield('content')
-        
-        
+
+
         <div class="modal fade" id="ModalEdit" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="ModalEdit" aria-hidden="true">
             <div class="modal-dialog modal-lg" role="document" id = "ModalContentForm" >
                 <div class="content_modal modal-content">
@@ -48,7 +48,7 @@
         </div>
         <script>
             $(document).ready(function () {
-
+                $('[data-toggle="tooltip"]').tooltip();  
             })
         </script>
     </body>
