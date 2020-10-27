ebec57124e3d5bfffc88587475e90f2835d828eb
JFCreategica <juanfelipe@creategicalatina.com>
2020-10-27 10:14:36 -0500

    integracion google tag manager

--- resources/views/layout/general2.blade.php
+++ resources/views/layout/general2.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WV2GTLM');</script>
+      <!-- End Google Tag Manager -->
+
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>HUBSENA</title>
 
@@ -18,7 +26,7 @@
         <?php echo '<script type="text/javascript" src = "../js/dataTables.js?v='.date("Y-m-d H:i:s").'"></script>';?>
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <?php echo "<link rel='stylesheet' href='../css/generalPage.css?v=".date("Y-m-d H:i:s")."'>";?>
-        
+
         <?php echo '<script type="text/javascript" src = "../js/jquery-ui.min.js?v='.date("Y-m-d H:i:s").'"></script>';?>
         <?php echo '<script type="text/javascript" src = "../js/config.js?v='.date("Y-m-d H:i:s").'"></script>';?>
 
@@ -29,16 +37,20 @@
         <?php echo '<script type="text/javascript" src = "../js/jquery-ui.min.js?v='.date("Y-m-d H:i:s").'"></script>';?>
         <?php echo '<script type="text/javascript" src = "../js/bootstrap-datepicker.js?v='.date("Y-m-d H:i:s").'"></script>';?>
         <?php echo '<script type="text/javascript" src = "../js/jquery.creditCardValidator.js?v='.date("Y-m-d H:i:s").'"></script>';?>
-        
+
         <style>
-            
+
         </style>
     </head>
     <body style = 'overflow-y:scroll;'>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV2GTLM"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
         {{ csrf_field() }}
         @yield('content')
-        
-        
+
+
         <script>
             $(document).ready(function () {
                 nextPaso(0)
